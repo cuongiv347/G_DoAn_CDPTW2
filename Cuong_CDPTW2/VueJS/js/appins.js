028168d8e3e6fe94c05044925687cc4f32adaf7d
cuongiv347 <pccuong112@gmail.com>
2019-11-07 07:40:47 +0700

    Update rar module

--- Cuong_CDPTW2/VueJS/js/appins.js
+++
@@ -1,8 +0,0 @@
-var ins = new Vue({
-    el: '#ins',
-    data: {
-        message3: 'INSTAGRAM',
-        message4: 'We love posting photos of our coustomers having a good time'
-
-    }
-}) 
--- Cuong_CDPTW2/VueJS/js/appsub.js
+++
@@ -1,9 +0,0 @@
-var sub = new Vue({
-    el: '#sub',
-    data: {
-        message5: 'SUBSCRIBE',
-        message6: 'Get updates about new dishes and upcoming events',
-
-
-    }
-}) 
